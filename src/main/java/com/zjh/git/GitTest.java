fa5a3366db7c30277b165f0d7ab2610a3fddb7e9
zjjjjjhi <1528386505@qq.com>
2022-04-12 14:08:18 +0800

    master commit3

--- src/main/java/com/zjh/git/GitTest.java
+++ src/main/java/com/zjh/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("master test");
         System.out.println("hot-fix");
+        System.out.println("push-fix");
     }
 }
